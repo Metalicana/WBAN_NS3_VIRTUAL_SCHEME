dd6a44b9167fbfb0c1f7c823b15a4d9beae033f8
Abdul Mohaimen Al Radi <alradi9923@gmail.com>
2021-04-22 00:51:28 +0600

    Update GatewayApp.h

--- GatewayApp.h
+++ GatewayApp.h
@@ -2,6 +2,9 @@
 #define NS3_GATEWAY_APP_H
 
 #include <fstream>
+#include <map>
+#include <string>
+#include <vector>
 #include "ns3/log.h"
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
@@ -12,12 +15,27 @@
 #include "ns3/ipv4.h"
 #include "ns3/ipv4-static-routing-helper.h"
 #include "ns3/ipv4-list-routing-helper.h"
+#include "cryptopp/aes.h"
+#include "cryptopp/default.h"
+#include "cryptopp/osrng.h"
+#include "cryptopp/modes.h"
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/seckey.h"
+#include "cryptopp/randpool.h"
+#include "cryptopp/rdrand.h"
+#include "cryptopp/sha3.h"
+
 
 using namespace std;
 using namespace ns3;
+using namespace CryptoPP;
 namespace ns3
 {
 
+struct key_comb
+  {
+    SecByteBlock Kj,Kl,Skey;
+  };
 class GatewayApp : public Application 
 {
     
@@ -39,10 +57,12 @@ private:
   void HandleRead(Ptr<Socket> socket);
   Ptr<Packet> packetize (string str);
   Ptr<Socket> GetListeningSocket (void) const;
+  void ReadKeys();
+  void ReadDoctorIndices();
 
   void ScheduleTx (void);
   
-
+  
   Ptr<Socket>     speaker_socket;
   Ptr<Socket>     listener_socket;
   Address         m_peer;
@@ -55,6 +75,8 @@ private:
   uint16_t        port;
   std::list<Ptr<Socket> > m_socketList; 
   uint8_t         m_id;
+  map<string,int> doctors_index;
+  vector<key_comb> keys; 
 };
 }
 
