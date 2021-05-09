ae1413fc6605fed71dcdcacd8a48187397a3bfde
Abdul Mohaimen Al Radi <alradi9923@gmail.com>
2021-05-09 21:29:30 +0600

    Update main.cc

--- main.cc
+++ main.cc
@@ -15,10 +15,12 @@
 #include "DoctorRegisterApp.h"
 #include "GatewayApp.h"
 #include "PhoneApp.h"
+#include "SensorApp.h"
 
 #define DOCTOR_REGISTER 5
 #define GATEWAY 6
 #define PATIENT 7
+#define SENSOR 8
 
 using namespace std;
 using namespace ns3;
@@ -34,8 +36,12 @@ RxDrop (Ptr<const Packet> p)
 {
   NS_LOG_UNCOND ("RxDrop at " << Simulator::Now ().GetSeconds ());
 }
-
-
+void init( Ptr<SensorApp> s,Ptr<PhoneApp> p)
+{
+  //get the Ui
+  byte *buff = p->GetUi();
+  s->SendPing(buff);
+}
 int 
 main (int argc, char *argv[])
 {
@@ -47,6 +53,7 @@ main (int argc, char *argv[])
   LogComponentEnable("DoctorRegisterApp",LOG_LEVEL_ALL);
   LogComponentEnable("GatewayApp",LOG_LEVEL_ALL);
   LogComponentEnable("PhoneApp",LOG_LEVEL_ALL);
+  LogComponentEnable("SensorApp",LOG_LEVEL_ALL);
   int doctorNodeCnt=5;
   int gatewayNodeCnt=1;
   int patientNodeCnt=3;
@@ -62,7 +69,7 @@ main (int argc, char *argv[])
   PointToPointHelper pointToPoint;
   pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
   pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
-  PointToPointStarHelper p2pStar(patientNodeCnt+sensorNodeCnt+doctorNodeCnt,pointToPoint);
+  PointToPointStarHelper p2pStar(totalNodes-1,pointToPoint);
 
 
 
@@ -85,19 +92,28 @@ main (int argc, char *argv[])
   
   Ptr<Socket> doctor_listener[doctorNodeCnt];
   Ptr<Socket> doctor_speaker[doctorNodeCnt];
+
   Ptr<Socket> patient_listener[patientNodeCnt];
   Ptr<Socket> patient_speaker[patientNodeCnt];
+
+  Ptr<Socket> sensor_listener[sensorNodeCnt];
+  Ptr<Socket> sensor_speaker[sensorNodeCnt];
+
   int i=0;
   for(int j=0;j<doctorNodeCnt;i++,j++)
   {
     doctor_listener[j] = Socket::CreateSocket ( p2pStar.GetSpokeNode(i),TcpSocketFactory::GetTypeId ());
     doctor_speaker[j]  = Socket::CreateSocket ( p2pStar.GetSpokeNode(i),TcpSocketFactory::GetTypeId ());
   }
-  i++;
   for(int j=0;j<patientNodeCnt;i++,j++)
   {
     patient_listener[j] = Socket::CreateSocket ( p2pStar.GetSpokeNode(i),TcpSocketFactory::GetTypeId ());
     patient_speaker[j]  = Socket::CreateSocket ( p2pStar.GetSpokeNode(i),TcpSocketFactory::GetTypeId ());
+  }
+  for(int j=0;j<sensorNodeCnt;j++,i++)
+  {
+    sensor_listener[j] = Socket::CreateSocket ( p2pStar.GetSpokeNode(i),TcpSocketFactory::GetTypeId ());
+    sensor_speaker[j]  = Socket::CreateSocket ( p2pStar.GetSpokeNode(i),TcpSocketFactory::GetTypeId ());
   }
   Ptr<Socket> gw_listener = Socket::CreateSocket (p2pStar.GetHub(), TcpSocketFactory::GetTypeId ());
   Ptr<Socket> gw_speaker = Socket::CreateSocket (p2pStar.GetHub(), TcpSocketFactory::GetTypeId ());
@@ -105,6 +121,8 @@ main (int argc, char *argv[])
   Address doctorAddress[doctorNodeCnt];
   Address patientAddress[patientNodeCnt];
   Address gwAddress[totalNodes-1];
+  Address sensorAddress[sensorNodeCnt];
+
   for(int j=0;j<totalNodes-1;j++)
   {
     gwAddress[j] = InetSocketAddress(p2pStar.GetHubIpv4Address(j), sinkPort);
@@ -114,14 +132,20 @@ main (int argc, char *argv[])
   {
     doctorAddress[j]=InetSocketAddress (p2pStar.GetSpokeIpv4Address (i), sinkPort);
   }
-  
   for(int j=0;j<patientNodeCnt;i++,j++)
   {
     patientAddress[j] =InetSocketAddress (p2pStar.GetSpokeIpv4Address (i), sinkPort);
   }
+  for(int j=0;j<sensorNodeCnt;i++,j++)
+  {
+    sensorAddress[j] =InetSocketAddress (p2pStar.GetSpokeIpv4Address (i), sinkPort);
+  }
 
   Ptr<DoctorRegisterApp> doctor_app[doctorNodeCnt];
   Ptr<PhoneApp> phone_app[patientNodeCnt];
+  Ptr<SensorApp> sensor_app[sensorNodeCnt];
+  Ptr<GatewayApp> gw_app = CreateObject<GatewayApp> ();
+
   i=0;
   for(int j=0;j<doctorNodeCnt;i++,j++)
   {
@@ -135,20 +159,72 @@ main (int argc, char *argv[])
   {
     phone_app[j] = CreateObject<PhoneApp> ();
     phone_app[j]->SetStartTime (Seconds (1.));
-    phone_app[i]->SetStopTime (Seconds (20.));
+    phone_app[j]->SetStopTime (Seconds (20.));
     phone_app[j]->Setup (patient_speaker[j],patient_listener[j], gwAddress[i]  , 1040, 1000, DataRate ("1Mbps"),sinkPort,PATIENT);
     (p2pStar.GetSpokeNode(i))->AddApplication(phone_app[j]);
   }
-  
+  for(int j=0;j<sensorNodeCnt;j++,i++)
+  {
+    sensor_app[j] = CreateObject<SensorApp> ();
+    sensor_app[j]->SetStartTime (Seconds (1.));
+    sensor_app[j]->SetStopTime (Seconds (20.));
+    sensor_app[j]->Setup (sensor_speaker[j],sensor_listener[j], gwAddress[i]  , 1040, 1000, DataRate ("1Mbps"),sinkPort,SENSOR);
+    (p2pStar.GetSpokeNode(i))->AddApplication(sensor_app[j]);
+  }
 
-  Ptr<GatewayApp> gw_app = CreateObject<GatewayApp> ();
+  
   gw_app->SetStartTime (Seconds (1.));
   gw_app->SetStopTime (Seconds (20.));
   gw_app->Setup (gw_speaker,gw_listener, p2pStar.GetHubIpv4Address(0) , 1040, 1000, DataRate ("1Mbps"),sinkPort,GATEWAY,10);
   (p2pStar.GetHub())->AddApplication(gw_app);
 
+  i=doctorNodeCnt;
+  //doctorNodeCnt+patientNodeCnt is where sensors starts
+  int bs = doctorNodeCnt+patientNodeCnt;
+  for(int j=0;j<patientNodeCnt;j++,i++)
+  {
+    for(int k=0;k<3;k++)
+    {
+      pointToPoint.Install(p2pStar.GetSpokeNode(i),p2pStar.GetSpokeNode(bs + j*3 + k));
+    }
+  }
+
+  //patient sends request to be registered 
+  //for each patient randomly pick an ID ui and perform SHA256 with the user ID of the gateway send it to the patient.
   //doctor_app[0]->StartSending("591022132\nabcdefgh");
-  Simulator::Schedule (Seconds (2), &DoctorRegisterApp::SendRegisterInfo, doctor_app[0], "1000" , "abcde", gwAddress[0]);
+  Simulator::Schedule (Seconds (3), &DoctorRegisterApp::SendRegisterInfo, doctor_app[0], "1000" , "abcde", gwAddress[0]);
+  Simulator::Schedule (Seconds (4), &DoctorRegisterApp::SendRegisterInfo, doctor_app[1], "2000" , "abcde", gwAddress[1]);
+  Simulator::Schedule (Seconds (5), &DoctorRegisterApp::SendRegisterInfo, doctor_app[2], "3000" , "abcde", gwAddress[2]);
+
+
+  Simulator::Schedule (Seconds (6), &PhoneApp::SendPing, phone_app[0]);
+  Simulator::Schedule (Seconds (7), &PhoneApp::SendPing, phone_app[1]);
+  Simulator::Schedule (Seconds (8), &PhoneApp::SendPing, phone_app[2]);
+  byte *buff;
+  for(i=0;i<patientNodeCnt;i++)
+  {
+    for(int j=0;j<3;j++)
+    {
+      //sensor node will ping the server with UI of the patient
+      //server will handle it
+      
+      Simulator::Schedule (Seconds (10.0 + (double)(j+0.25)), &init, sensor_app[i*patientNodeCnt+j], phone_app[i]);
+    }
+  }
+  /*byte *buff;
+  byte temp[16];
+  for(i=0;i<patientNodeCnt;i++)
+  {
+    buff = phone_app[i]->GetUi();
+    for(int j=0;j<sensorNodeCnt;j++)
+    {
+      sensor_app[i*sensorNodeCnt + j]->SetBuff(buff);
+    }
+  }*/
+  
+
+
+
 
 
 //   Ipv4AddressHelper address;
