8f60a17f2c1044c0c4c05dfac6f9f0d81c7afd3e
Abdul Mohaimen Al Radi <alradi9923@gmail.com>
2021-05-09 21:30:13 +0600

    Update PhoneApp.h

--- PhoneApp.h
+++ PhoneApp.h
@@ -12,6 +12,17 @@
 #include "ns3/ipv4.h"
 #include "ns3/ipv4-static-routing-helper.h"
 #include "ns3/ipv4-list-routing-helper.h"
+#include "cryptopp/aes.h"
+#include "cryptopp/default.h"
+#include "cryptopp/osrng.h"
+#include "cryptopp/modes.h"
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/seckey.h"
+#include "cryptopp/randpool.h"
+#include "cryptopp/rdrand.h"
+#include "cryptopp/sha3.h"
+#include "cryptopp/hex.h"
+#include "cryptopp/files.h"
 
 using namespace std;
 using namespace ns3;
@@ -25,10 +36,12 @@ public:
     
   PhoneApp ();
   virtual ~PhoneApp();
+  void SendPing();
   void SendPacket (Ptr<Packet> packet);
   void StartSending(string g);
   void RecvString(Ptr<Socket> sock);
   void SendRegisterInfo(string Mid, string PW, Address add);
+  byte* GetUi();
   void Setup (Ptr<Socket> _speaker_socket, Ptr<Socket> _listener_socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate,uint16_t _port, uint8_t _m_id);
 
 private:
@@ -54,6 +67,7 @@ private:
   uint16_t        port;
   std::list<Ptr<Socket> > m_socketList; 
   uint8_t         m_id;
+  byte            ui[16];
 };
 
 }
