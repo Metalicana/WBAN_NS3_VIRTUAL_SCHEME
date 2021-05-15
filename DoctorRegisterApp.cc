1b9e7244a5747df34019cac84cdb79e90a00d25a
Abdul Mohaimen Al Radi <alradi9923@gmail.com>
2021-05-15 07:42:16 +0600

    Update DoctorRegisterApp.cc

--- DoctorRegisterApp.cc
+++ DoctorRegisterApp.cc
@@ -1,5 +1,9 @@
 
 #include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
 #include "ns3/log.h"
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
@@ -10,8 +14,24 @@
 #include "ns3/ipv4.h"
 #include "ns3/ipv4-static-routing-helper.h"
 #include "ns3/ipv4-list-routing-helper.h"
-#include "ns3/inet-socket-address.h"
+#include "ns3/attribute.h"
+#include "ns3/attribute-helper.h"
+
+#include "cryptopp/aes.h"
+#include "cryptopp/default.h"
+#include "cryptopp/osrng.h"
+#include "cryptopp/modes.h"
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/seckey.h"
+#include "cryptopp/randpool.h"
+#include "cryptopp/rdrand.h"
+#include "cryptopp/sha3.h"
+#include "cryptopp/hex.h"
+#include "cryptopp/files.h"
+
 #include "DoctorRegisterApp.h"
+#include "SensorApp.h"
+#include "PhoneApp.h"
 
 using namespace ns3;
 using namespace std;
@@ -27,6 +47,7 @@ using namespace std;
 #define BOLD_CODE "\033[1m"
 #define END_CODE "\033[0m"
 
+#define DOCTOR_LOGIN 4
 #define DOCTOR_REGISTER 5
 #define GATEWAY 6
 
@@ -149,12 +170,112 @@ void DoctorRegisterApp::SendRegisterInfo(string Mid, string PW, Address add)
   char x = (char)(DOCTOR_REGISTER + '0');
   string g = x + Mid  + '\n' + PW;
   Ptr<Packet> p = packetize(g);
+
+  Ptr<Socket> speak = Socket::CreateSocket (GetNode(), TcpSocketFactory::GetTypeId ());
+
+  speak->Bind ();
+  speak->Connect(add);
+  
+  speak->Send (p);
+ // speaker_socket->ShutdownSend();
+  NS_LOG_INFO(BLUE_CODE << "Successfully sent Mid and PW to GW" << END_CODE);
+
+}
+void DoctorRegisterApp::SendLoginInfo(string Mid, string PW, Address add, Ptr<PhoneApp> patient, Ptr<SensorApp> sensor)
+{
+  char x = (char)(DOCTOR_REGISTER + '0');
+  string g = x + Mid  + '\n' + PW;
+
+  string digest1,digest2,digest3;
+
+  SHA3_256 hash;
+  
+  byte buff[32];
+  for(int i=0;i<(int)Mid.size();i++)buff[i]=(byte)Mid[i];
+  hash.Update(buff, (int)Mid.size());
+  digest1.resize(hash.DigestSize());
+  hash.Final((byte*)&digest1[0]);
+  
+  for(int i=0;i<(int)PW.size();i++)buff[i]=(byte)PW[i];
+
+  hash.Update(buff, (int)PW.size());
+  digest2.resize(hash.DigestSize());
+  hash.Final((byte*)&digest2[0]);
+
+  hash.Update(Skey, AES::BLOCKSIZE);
+  digest3.resize(hash.DigestSize());
+  hash.Final((byte*)&digest3[0]);
+
+  byte hash_string[(int)digest1.size()];
+  for(int i=0;i<(int)digest1.size();i++)
+  {
+    hash_string[i] = (byte)(digest1[i]^digest2[i]^digest3[i]);
+  }
+  for(int i=0;i<32;i++)
+  {
+    if((int)hash_string[i] != (int)Ni[i] )
+    {
+      NS_LOG_INFO(GREEN_CODE << "WRONG CREDENTIALS. LOGIN FAILED" << END_CODE);
+      return;
+    }
+  }
+
+  
+  //digest1 is H(Mid)
+  AutoSeededRandomPool rnd;
+  SecByteBlock M(0x00, AES::DEFAULT_KEYLENGTH);
+  rnd.GenerateBlock(M,M.size());
+  byte *Mb = M.data();
+  byte *Ui = patient->GetUi();
+  byte *Nj = sensor->GetSNj();
+  byte CIDi[10000];
+  Time cur = Simulator::Now();
+  TimeValue tv(cur);
+  Ptr<AttributeChecker> check;
+  g = tv.SerializeToString(check);
+
+  SecByteBlock key(Kl, AES::DEFAULT_KEYLENGTH);
+  SecByteBlock iv(KlIV,AES::BLOCKSIZE);
+  cout << Simulator::Now() << endl;
+  int i=0;
+  for(int j=0;j<32;j++,i++)CIDi[i]=(byte)digest1[j];
+  for(int j=0;j<16;j++,i++)CIDi[i] = Mb[j];
+  for(int j=0;j<16;j++,i++)CIDi[i] = Ui[j];
+  for(int j=0;j<16;j++,i++)CIDi[i] = Nj[j];
+  CIDi[i++]=g.size();
+  for(int j=0;j<g.size();j++,i++)CIDi[i]=(byte)g[j];
+
+  CFB_Mode<AES>::Encryption cfbEncryption(key, key.size(), iv);
+  cfbEncryption.ProcessData(CIDi, CIDi, (size_t)(i+1) );
+
+  byte message[10000];
+  message[0] =DOCTOR_LOGIN+'0';
+  message[1] = 3;
+  message[2] = i;
+  int sz = i;
+  i=3;
+  for(int j=0;j<sz;j++,i++)message[i]=CIDi[j];
+  message[i++]=6;
+  for(int j=0;j<6;j++,i++)message[i] = C[j];
+  message[i++]=g.size();
+  for(int j=0;j<(int)g.size();j++,i++)message[i]=(byte)g[j];
+  
+  Ptr<Packet> p = Create<Packet>(message,i);
+  Ptr<Socket> speak = Socket::CreateSocket (GetNode(), TcpSocketFactory::GetTypeId ());
+  speak->Bind();
+  speak->Connect(m_peer);
+  speak->Send(p);
+  speak->ShutdownSend();
+  NS_LOG_INFO(PURPLE_CODE << "Successfully sent Login info to GW" << END_CODE);
+
+
+ /* Ptr<Packet> p = packetize(g);
   speaker_socket->Bind ();
   speaker_socket->Connect(add);
   
   speaker_socket->Send (p);
   speaker_socket->ShutdownSend();
-  NS_LOG_INFO(BLUE_CODE << "Successfully sent Mid and PW to GW" << END_CODE);
+  NS_LOG_INFO(BLUE_CODE << "Successfully sent Mid and PW to GW" << END_CODE);*/
 
 }
 void DoctorRegisterApp::
@@ -185,6 +306,14 @@ RecvString(Ptr<Socket> sock)//Callback
         cout << prev << ": ";
         for(int j=0;j<prev;i++,j++)
         {
+          if(q==0)C[j]=data[i];
+          else if(q==1)Ni[j]=data[i];
+          else if(q==2)Skey[j]=data[i];
+          else if(q==3)SkeyIV[j]=data[i];
+          else if(q==4)Kj[j]=data[i];
+          else if(q==5)KjIV[j]=data[i];
+          else if(q==6)Kl[j]=data[i];
+          else KlIV[j]=data[i];
           cout << (int)data[i] << " ";
         }
         cout << endl;
