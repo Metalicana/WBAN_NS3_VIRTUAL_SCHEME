e69d371387373fb79814ab6306ef33d5edc6ef8f
Abdul Mohaimen Al Radi <alradi9923@gmail.com>
2021-05-15 07:42:33 +0600

    Update DoctorRegisterApp.h

--- DoctorRegisterApp.h
+++ DoctorRegisterApp.h
@@ -12,9 +12,23 @@
 #include "ns3/ipv4.h"
 #include "ns3/ipv4-static-routing-helper.h"
 #include "ns3/ipv4-list-routing-helper.h"
+#include "cryptopp/aes.h"
+#include "cryptopp/default.h"
+#include "cryptopp/osrng.h"
+#include "cryptopp/modes.h"
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/seckey.h"
+#include "cryptopp/randpool.h"
+#include "cryptopp/rdrand.h"
+#include "cryptopp/sha3.h"
+#include "cryptopp/hex.h"
+#include "cryptopp/files.h"
+#include "SensorApp.h"
+#include "PhoneApp.h"
 
 using namespace std;
 using namespace ns3;
+using namespace CryptoPP;
 namespace ns3
 {
   class DoctorRegisterApp : public Application 
@@ -29,6 +43,7 @@ public:
   void StartSending(string g);
   void RecvString(Ptr<Socket> sock);
   void SendRegisterInfo(string Mid, string PW, Address add);
+  void SendLoginInfo(string Mid, string PW, Address add,Ptr<PhoneApp> patient, Ptr<SensorApp> sensor);
   void Setup (Ptr<Socket> _speaker_socket, Ptr<Socket> _listener_socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate,uint16_t _port, uint8_t _m_id);
 
 private:
@@ -54,6 +69,18 @@ private:
   uint16_t        port;
   std::list<Ptr<Socket> > m_socketList; 
   uint8_t         m_id;
+  byte            Ni[32];
+  byte            C[6];
+
+  byte            Skey[16];
+  byte            SkeyIV[16];
+  byte            Kj[16];
+  byte            KjIV[16];
+  byte            Kl[16];
+  byte            KlIV[16];
+
+
+  
 };
 
 }
