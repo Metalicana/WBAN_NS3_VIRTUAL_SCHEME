d4a6a77b4dc66802fa4a3a42d2466ccb569b1019
Abdul Mohaimen Al Radi <alradi9923@gmail.com>
2021-05-16 07:28:11 +0600

    Update PhoneApp.cc

--- PhoneApp.cc
+++ PhoneApp.cc
@@ -11,6 +11,18 @@
 #include "ns3/ipv4-static-routing-helper.h"
 #include "ns3/ipv4-list-routing-helper.h"
 
+#include "cryptopp/aes.h"
+#include "cryptopp/default.h"
+#include "cryptopp/osrng.h"
+#include "cryptopp/modes.h"
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/seckey.h"
+#include "cryptopp/randpool.h"
+#include "cryptopp/rdrand.h"
+#include "cryptopp/sha3.h"
+#include "cryptopp/hex.h"
+#include "cryptopp/files.h"
+
 #include "PhoneApp.h"
 
 using namespace ns3;
@@ -67,6 +79,7 @@ PhoneApp::Setup (Ptr<Socket> _speaker_socket, Ptr<Socket> _listener_socket, Addr
   m_dataRate = dataRate;
   port = _port;
   m_id = _m_id;
+  sensor_cnt=0;
 }
 
 void
@@ -176,23 +189,172 @@ RecvString(Ptr<Socket> sock)//Callback
       uint8_t data[255];
       packet->CopyData(data,sizeof(data));//Write the data in the package into data
       //cout <<sock->GetNode()->GetId()<<" "<<"receive : '" << data <<"' from "<<address.GetIpv4 ()<< endl;  
-      int num = (int)data[0];
+      int tp = (int)data[0];
+      int num = (int)data[1];
       int prev;
-      int i=1;
-      for(int q=0;q<num;q++)
+      int i=2;
+      byte Vip[1000];
+      byte TT[100];
+      int totalsz;
+      int TTsz;
+      if(tp==3)
       {
-        prev = (int)data[i];
-        i++;
-        //cout << prev << ": ";
-        for(int j=0;j<prev;i++,j++)
+        for(int q=0;q<num;q++)
         {
-          //cout << (int)data[i] << " ";
-          if(q==0)Ui[j]=data[i];
-          else if(q==1)KGWU[j]=data[i];
+            prev = (int)data[i];
+            if(q==0)totalsz=prev;
+            else TTsz=prev;
+            i++;
+            cout << prev << ": ";
+            for(int j=0;j<prev;i++,j++)
+            {
+              cout << (int)data[i] << " ";
+              if(q==0)Vip[j]=data[i];
+              else TT[j]=data[i];
+              
+            }
+            cout << endl;
         }
-        //cout << endl;
+        byte a[16],b[16];
+        for(int q=0;q<16;q++)a[q]=KGWU[q];
+        for(int q=0,w=16;q<16;q++,w++)b[q]=KGWU[w];
+
+        SecByteBlock chubby(a,AES::DEFAULT_KEYLENGTH);
+        SecByteBlock chubbyIV(b,AES::BLOCKSIZE);
+        CFB_Mode<AES>::Decryption dc(chubby, chubby.size(), chubbyIV);
+        dc.ProcessData(Vip, Vip, (size_t)(totalsz+1) );
+
+        byte Mid[10],Uip[16],SNjp[16],M[16],curTime[100];
+        i=0;
+        for(int q=0;q<5;q++)
+        {
+          if(q==0)prev=4;
+          else if(q<4)prev=16;
+          else prev=TTsz,i++;
+          cout << prev << ": ";
+          for(int w=0;w<prev;w++,i++)
+          {
+            if(q==0)Mid[w]=Vip[i];
+            else if(q==1)Uip[w]=Vip[i];
+            else if(q==2)SNjp[w]=Vip[i];
+            else if(q==3)M[w]=Vip[i];
+            else curTime[w]=Vip[i];
+            cout << (int)Vip[i] << " ";
+          }
+          cout << endl;
+        }
+        string tempTime="";
+        for(int q=0;q<TTsz;q++)tempTime+=(char)curTime[q];
+        Time cur = Simulator::Now();
+        TimeValue tv(cur);
+        Ptr<AttributeChecker> check;
+        tv.DeserializeFromString(tempTime,check);
+        Time jeez = tv.Get();
+        Time christ = Simulator::Now();
+        Time comp = NanoSeconds(50000000);
+        if(christ-jeez > comp)
+        {
+          NS_LOG_INFO(RED_CODE << "PACKET DELAY LIMIT REACHED"<<END_CODE);
+          return;
+        }
+        for(int q=0;q<16;q++)
+        {
+          if(Uip[q]!=Ui[q])
+          {
+             NS_LOG_INFO(RED_CODE << "DATA CORRUPTED!"<<END_CODE);
+            return;
+          }
+        }
+
+        Time cur2 = Simulator::Now();
+        TimeValue tv2(cur2);
+        Ptr<AttributeChecker> check2;
+        string g = tv2.SerializeToString(check2);
+
+
+        i=0;
+        for(int q=0;q<5;q++)
+        {
+          if(q==0)prev=4;
+          else if(q<4)prev=16;
+          else prev=(int)g.size(),Vip[i++]=prev;
+          for(int w=0;w<prev;w++,i++)
+          {
+            if(q==0)Vip[i]=Mid[w];
+            else if(q==1)Vip[i]=Uip[w];
+            else if(q==2)Vip[i]=SNjp[w];
+            else if(q==3)Vip[i]=M[w];
+            else Vip[i]=(byte)g[w];
+          }
+        }
+
+        int totesz = i;
+        string h = g;
+        byte message[1000];
+        g="";
+        for(int q=0;q<16;q++)a[q]=KUSNj[q];
+        for(int q=0,w=16;q<16;q++,w++)b[q]=KUSNj[w];
+        for(int q=0;q<16;q++)g+=(char)SNjp[q];
+        SecByteBlock chubby2(a,AES::DEFAULT_KEYLENGTH);
+        SecByteBlock chubby2IV(b,AES::BLOCKSIZE);
         
+        CFB_Mode<AES>::Encryption ec(chubby2, chubby2.size(), chubby2IV);
+        ec.ProcessData(Vip, Vip, (size_t)(totesz+1) );
+        int idx = sensor_idx[g];
+        NS_LOG_INFO(GREEN_CODE<<idx<<END_CODE);
+        Address add = sensor_address[idx];
+
+        message[0]=2;
+        message[1]=2;
+        message[2]=totesz;
+        i=3;
+        for(int q=0;q<totesz;q++,i++)message[i]=Vip[q];
+        message[i++]=h.size();
+        for(int q=0;q<(int)h.size();q++)message[i++]=h[q];
+
+        Ptr<Socket> speak2 = Socket::CreateSocket (GetNode(), TcpSocketFactory::GetTypeId ());
+        Ptr<Packet> p = Create<Packet>(message,i);
+        speak2->Bind();
+        speak2->Connect(add);
+        speak2->Send(p);
+        speak2->ShutdownSend();
+        NS_LOG_INFO("Data sent to unknown person");
+
       }
+      else
+      {
+          for(int q=0;q<num;q++)
+          {
+            prev = (int)data[i];
+            i++;
+            cout << prev << ": ";
+            for(int j=0;j<prev;i++,j++)
+            {
+              cout << (int)data[i] << " ";
+              if(tp==1)
+              {
+                if(q==0)Ui[j]=data[i];
+                else if(q==1)KGWU[j]=data[i];
+              }
+              else if(tp==2)
+              {
+                if(q==0)SNj[j]=data[i];
+                else if(q==1)KUSNj[j]=data[i];
+              }
+              
+            }
+            cout << endl;
+            
+          }
+          if(tp==2)
+          {
+            string g="";
+            for(int q=0;q<16;q++)g+=(char)SNj[q];
+            sensor_idx[g]=sensor_cnt;
+            sensor_cnt++;
+          }
+      }
+      
     }
     
  
@@ -204,6 +366,10 @@ void PhoneApp::SendPing()
   s += (char)(PATIENT + '0');
   Ptr<Packet> p = packetize(s);
   SendPacket(p);
+}
+void PhoneApp::UpdateSensor(Address ad)
+{
+  sensor_address.push_back(ad);
 }
 void PhoneApp::HandleAccept (Ptr<Socket> s, const Address& from)
  {
